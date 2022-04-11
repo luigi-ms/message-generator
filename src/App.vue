d36132c2c3d7248f9ed80ae34ee6511679c0fa9c
Luigi Moraes <luigimoraes3@gmail.com>
2022-04-11 14:55:30 -0300

    logic done. style started

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="talkField">
+		<header>
+			<font-awesome-icon icon="AngleLeft" />
+			<h2>Grupo random</h2>
+		</header>
+  
+		<ul>
+			<MessageComponent v-for="message in messages" v-bind:key="message.msg" :msg="message" />
+		</ul>
+	</div>
+	
+	<form>
+		<input v-model="user" placeholder="Nome do usuario" />
+		<input v-model="msg" placeholder="Mensagem"/>
+		
+		<button v-on:click.prevent="add">Adicionar mensagem</button>
+	</form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MessageComponent from './components/Message.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+	components: { MessageComponent },
+  data: () => {
+		return {
+			messages: [{ userName: "Luigi", text: "Salve", now: "13:47" }],
+			user: "",
+			msg: ""
+		}
+	},
+	methods: {
+		add: function(){
+			const time = new Date();
+
+			this.messages.push(
+				{ userName: this.user, 
+					text: this.msg, 
+					now: `${time.getHours()} : ${time.getMinutes()}` 
+				});
+		}
+	}
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-size: 0.9em;
+}
+
+#talkField {
+	background-color: #041C32;;
+	color: #ECB365;
+}
+
+header {
+	background-color: #1a1b26; 
+	display: flex;
+	justify-content: center;
+	padding: 2.5%;
+}
+
+form {
+	display: flex;
+	flex-direction: column;
+	margin-top: 2vh;
+}
+
+input {
+	width: 50vw;
+	margin-bottom: 1vh;
+}
+
+button { width: 25vw; }
+
+ul {
+	background-color: #041C32;
+	padding: 0;
+	padding: 5%;
 }
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,37 @@
+<template>
+	<li>
+		<h3>{{ msg.userName }}</h3>
+		<p id="text">{{ msg.text }}</p>
+		<p id="time">{{ msg.now }}</p>
+	</li>
+</template>
+
+<script>
+export default {
+  name: 'MessageComponent',
+  props: { msg: Object }
+}
+</script>
+
+<style scoped>
+	* {
+		font-size: inherit;
+	}
+
+	li {
+		display: flex;
+		flex-direction: column;
+		padding: 0.2% 2.5% 1% 2.5%;
+		border: 1px dotted #ECB365;
+		list-style: none;
+		margin-bottom: 2%;
+	}
+	
+	p {
+		margin: 0;	
+	}
+
+	#time {
+		align-self: end;
+	}
+</style>
